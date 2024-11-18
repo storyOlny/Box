699d3afddf8c7d1add4c5dedbdac791c2f0176a8
1810883212@qq.com <1810883212@qq.com>
2024-11-18 19:55:18 +0800

    [fix] 添加注释

--- box-demo/src/main/java/com/jyz/demo/smsTemplate/controller/SmstemplateController.java
+++ box-demo/src/main/java/com/jyz/demo/smsTemplate/controller/SmstemplateController.java
@@ -3,6 +3,7 @@ package com.jyz.demo.smsTemplate.controller;
 import box.template.MessageSmsTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.ExecutionException;
@@ -13,15 +14,20 @@ public class SmstemplateController {
     @Autowired
     private MessageSmsTemplate messageSmsTemplate;
 
-    @GetMapping("/sendMessage")
-    public String sendMessage() {
+    /**
+     * 短信测试接口，param参数可以自定义，但是要和模板中的参数对应上
+     * @param number 手机号码
+     * @return
+     */
+    @PostMapping("/sendMessage")
+    public String sendMessage(String number) {
         try {
-            messageSmsTemplate.sendMessage("13279238822", "{\"code\":\"0727\"}");
+            messageSmsTemplate.sendMessage(number, "{\"code\":\"0727\"}");
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return "sendMessage";
+        return "sendMessage success.";
     }
 }
